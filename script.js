ed4b1f78ec0b7c68f6b9657077ca984e72b20baa
Saad E <21321555+se447@users.noreply.github.com>
2018-12-26 16:37:13 -0500

    Created function that calculates tip and pushes to an array

--- script.js
+++ script.js
@@ -1,37 +1,17 @@
 /*****************************
-* CODING CHALLENGE 2
+* CODING CHALLENGE 3
 */
 
 /*
-John and Mike both play basketball in different teams. In the latest 3 games, John's team scored 89, 120 and 103 points, while Mike's team scored 116, 94 and 123 points.
+John and his family went on a holiday and went to 3 different restaurants. The bills were $124, $48 and $268.
 
-1. Calculate the average score for each team
-2. Decide which teams wins in average (highest average score), and print the winner to the console. Also include the average score in the output.
-3. Then change the scores to show different winners. Don't forget to take into account there might be a draw (the same average score)
+To tip the waiter a fair amount, John created a simple tip calculator (as a function). He likes to tip 20% of the bill when the bill is less than $50, 15% when the bill is between $50 and $200, and 10% if the bill is more than $200.
 
-4. EXTRA: Mary also plays basketball, and her team scored 97, 134 and 105 points. Like before, log the average winner to the console. HINT: you will need the && operator to take the decision. If you can't solve this one, just watch the solution, it's no problem :)
-5. Like before, change the scores to generate different winners, keeping in mind there might be draws.
+In the end, John would like to have 2 arrays:
+1) Containing all three tips (one for each bill)
+2) Containing all three final paid amounts (bill + tip).
 
-GOOD LUCK 😀
+(NOTE: To calculate 20% of a value, simply multiply it with 20/100 = 0.2)
+
+GOOD LUCK 
 */
-
-let averageJohn = (89 + 120 + 103) / 3;
-let averageMike = (116 + 94 + 123) / 3;
-let averageMary = (97 + 134 + 105) / 3;
-
-averageJohn > averageMike ? 'John won with a score of ' + averageJohn + ' points'
-  : 'Mike won with a score of ' + averageMike + ' points';
-
-switch (true) {
-  case (averageJohn > averageMike && averageJohn > averageMary):
-    console.log('John won with a score of ' + averageJohn);
-    break;
-  case (averageMike > averageJohn && averageMike > averageMary):
-    console.log('Mike won with a score of ' + averageMike);
-    break;
-  case (averageMary > averageMike && averageMary > averageJohn):
-    console.log('Mary won with a score of ' + averageMary);
-    break;
-  default:
-    console.log('There is a tie');
-}
